78e53f1a1843f203c83269e521beb3dbc16d4d2e
arunj <arunj@AcerAspire5>
2023-09-28 22:55:10 +0530

    hompage

--- src/test/java/main/pack/test/one/HomePage.java
+++ src/test/java/main/pack/test/one/HomePage.java
@@ -15,6 +15,10 @@ public class HomePage {
   public void f() {
 	  
 	  System.out.println("hello");
+	  
+	  for(int i = 0; i <=10; i++) {
+		System.out.println(i);  
+	  }
   }
   @BeforeMethod
   public void beforeMethod() {
